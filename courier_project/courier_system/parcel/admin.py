6c8cbed8298453044f422b8d1d85296e2cc8b92c
KafiAhmed09 <137321243+KafiAhmed09@users.noreply.github.com>
2025-05-12 14:48:13 +0600

    Update admin.py

--- courier_project/courier_system/parcel/admin.py
+++ courier_project/courier_system/parcel/admin.py
@@ -5,5 +5,5 @@ from django.contrib import admin
 from .models import Merchant, Parcel, Order
 
 admin.site.register(Merchant)
-admin.site.register(Parcel)  # Ensure this line is included
+admin.site.register(Parcel)  
 admin.site.register(Order)
