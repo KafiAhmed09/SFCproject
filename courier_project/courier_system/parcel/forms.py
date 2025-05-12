c10ab606b64bf7188dfe5f41273f7dd270fb39f5
KafiAhmed09 <137321243+KafiAhmed09@users.noreply.github.com>
2025-05-12 14:51:24 +0600

    Update forms.py

--- courier_project/courier_system/parcel/forms.py
+++ courier_project/courier_system/parcel/forms.py
@@ -6,8 +6,7 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ['parcel', 'location']
 
-    # You can override the init method to customize the form behavior
+   
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Limit the available parcels to those that are active or match certain criteria
         self.fields['parcel'].queryset = Parcel.objects.all()
