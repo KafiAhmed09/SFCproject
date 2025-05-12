9de2f351b3bba06c87049f2e0cfa4ed560188cce
KafiAhmed09 <137321243+KafiAhmed09@users.noreply.github.com>
2025-05-12 14:53:05 +0600

    Update urls.py

--- courier_project/courier_system/parcel/urls.py
+++ courier_project/courier_system/parcel/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create_order, name='create_order'),
-    path('order-summary/<int:pk>/', views.order_summary, name='order_summary'),  # Added this line
+    path('order-summary/<int:pk>/', views.order_summary, name='order_summary'),  
 ]
